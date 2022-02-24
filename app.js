06fb0f52218dc0734d30c2f83677fcaa5318358b
phatb1906740 <95117726+phatb1906740@users.noreply.github.com>
2022-02-24 15:11:56 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const res = require("express/lib/response");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/", (req,res) => {
+    res.json({message: "Welcome to contact book application."});
+});
+
+module.exports = app;
